8b63313add690b125d0c66607644e712256207dc
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-01-13 00:44:15 -0300

    Update CalculoFrigorias.js
    Se agrega funcion dosDecimales: trunca el resultado del numero obtenido a dos decimales (request de Aracelli Trucco)
    La funcion dosDecimales fue dada por el Sr Pablo Lozano en Stack Overflow

--- js/CalculoFrigorias.js
+++ js/CalculoFrigorias.js
@@ -1,3 +1,9 @@
+//funcion dos decimales por Pablo Lozano: perfil StackOverflow:https://es.stackoverflow.com/users/22851/pablo-lozano
+function dosDecimales(n) { 
+    let t=n.toString();
+    let regex=/(\d*.\d{0,2})/;
+    return t.match(regex)[0];
+  }
 function calcularFrigorias(){
     largo = document.getElementById("largo").value;
     ancho = document.getElementById("ancho").value;
@@ -20,6 +26,7 @@ function calcularFrigorias(){
     }
     auxiliar = (largo * ancho) * alto;
     resultado = auxiliar * 50;
+    resultado=dosDecimales(resultado); //funcion que trunca en dos decimales (tomado de Stack Overflow:https://es.stackoverflow.com/questions/108268/c%C3%B3mo-truncar-un-numero-a-2-decimales-en-javascript respuesta 4 pablo lozano)
     document.CalcFrigorias.resultado.value = resultado;
     return true;
 }
