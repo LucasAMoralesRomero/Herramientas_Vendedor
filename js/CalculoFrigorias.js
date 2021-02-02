b3138fa6fffa108f18cf2a5ccbd6c78b475505e8
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-02-02 09:03:19 -0300

    Se agrega personas al calculo de frigorias, se elimina alertify de css ya que no se utiliza.

--- js/CalculoFrigorias.js
+++ js/CalculoFrigorias.js
@@ -8,6 +8,8 @@ function calcularFrigorias(){
     largo = document.getElementById("largo").value;
     ancho = document.getElementById("ancho").value;
     alto = document.getElementById("alto").value;
+    personas =document.getElementById("personas").value;
+    resultado_personas = 0;
     auxiliar = 0;
     resultado =0;
     if (largo == "")
@@ -24,8 +26,12 @@ function calcularFrigorias(){
     {
         alert('>ALERTA< El campo Alto no debe estar vacio');
     }
+    //calcular las frigorias para personas (a 150 frigorias x persona en el ambiente)
+    resultado_personas = personas * 150;
     auxiliar = (largo * ancho) * alto;
     resultado = auxiliar * 50;
+    //suma de frigorias base + frigorias de las personas
+    resultado = resultado + resultado_personas;
     resultado=dosDecimales(resultado); //funcion que trunca en dos decimales (tomado de Stack Overflow:https://es.stackoverflow.com/questions/108268/c%C3%B3mo-truncar-un-numero-a-2-decimales-en-javascript respuesta 4 pablo lozano)
     document.CalcFrigorias.resultado.value = resultado;
     return true;
